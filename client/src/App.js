a823656af4e21223615be598b23fa00545ce8179
Mike Henry <m.henry511@gmail.com>
2021-06-19 00:19:08 -0400

    First client fetch

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,21 @@
+import { useState, useEffect } from 'react'
+
 function App() {
+
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    fetch('lol?q=Boston')
+      .then(data => data.json())
+      .then(setData)
+  }, [])
+
   return (
     <div className="App">
       Tweeter
+      {
+        data ? JSON.stringify(data) : ''
+      }
     </div>
   );
 }
