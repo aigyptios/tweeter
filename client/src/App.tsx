91b2065a90a6ef80e804f7b69239851eb98082bc
Mike Henry <m.henry511@gmail.com>
2021-06-21 18:36:23 -0400

    Add Tweet component and quick way to test API

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
-import { isSuccessResponse, isErrorResponse, ITweet, IAPIResponse } from './types/types';
+import { Tweet } from './components/Tweet';
+import { isSuccessResponse, isErrorResponse, IAPIResponse } from './types/types';
 
 
 function App() {
@@ -7,14 +8,16 @@ function App() {
   const [data, setData] = useState<IAPIResponse>();
 
   useEffect(() => {
-    fetch('lol?q=the&result_type=popular&count=23')//&max_id=1405990040187113500')
+    const query = window.location.search || '?q=the&result_type=popular&count=23'
+    console.log(query)
+    fetch('tweets' + query )//&max_id=1405990040187113500')
       .then(data => data.json())
       .then(setData)
   }, [])
 
   return (
     <div className="App">
-      Tweeter
+      Twitter Feed
       { data && isSuccessResponse(data) && 
         <ol>
           {
@@ -30,40 +33,5 @@ function App() {
   );
 }
 
-function parseUrlFromText(text: string): string | undefined {
-  let regEx = /https:\/\/t.co\/.*/;
-  if (regEx.test(text))
-    return text.match(/https:\/\/t.co\/.*/)!![0]
-}
-
-
-type TweetProps = {
-  tweet: ITweet
-}
-function Tweet(props: TweetProps) {
-
-  const { tweet } = props;
-  const { user } = tweet;
-  const url = tweet.entities.urls?.[0]?.url || parseUrlFromText(tweet.text)
-
-  return <li style={{ marginBottom: 100 }}>
-    Faves: {tweet.favorite_count} | Retweets: {tweet.retweet_count}
-    <br />
-    {tweet.created_at}
-    <br />
-    {tweet.id}
-    <br />
-    {tweet.text}
-    <br />
-    {user.screen_name}
-    <br />
-    <img src={user.profile_image_url_https} alt={`${user.screen_name}'s Twitter avatar`} />
-    <br />
-    Hashtags: {JSON.stringify(tweet.entities.hashtags.map(ht => ht.text))}
-    <br />
-    {url && <a href={url}>{url}</a>}
-  </li>
-
-}
 
 export default App;
---
+++ client/src/test/Tweet.test.tsx
@@ -0,0 +1,51 @@
+import { cleanup, render, screen } from '@testing-library/react';
+import { Tweet, parseUrlFromText } from '../components/Tweet';
+import { ITweet } from '../types/types';
+
+describe("The Tweet Component", () => {
+
+  let mockTweet : ITweet = {
+    id: 10000001,
+    text: 'This is the tweet text and at the end of the tweet is sometimes a url like https://t.co/ABCdeFG87J',
+    entities: ({
+      urls: [{ url: 'https://t.co/ABCdeFG87J' }],
+      hashtags: [{text: 'HashtagLifeAmIRite' }]
+    }),
+    created_at: 'Sat Jun 19 04:34:01 +0000 2021',
+    favorite_count: 12345,
+    retweet_count: 54321,
+    user: {
+      screen_name: 'Chemistry73',
+      profile_image_url_https: 'https://pbs.twimg.com/profile_images/1329647526807543809/2SGvnHYV_normal.jpg',
+    }
+  }
+
+  beforeEach( () => {
+    render(<Tweet tweet={mockTweet}/>)
+  })
+
+  afterEach(()=> {
+    cleanup()
+  })
+  
+  test('displays the tweet text', () => {
+    const tweetText = screen.getByText(mockTweet.text);
+    expect(tweetText).toBeInTheDocument();
+  });
+
+  test('renders Tweet link', () => {
+    const tweetLink = screen.getByText('https://t.co/ABCdeFG87J');
+    expect(tweetLink).toBeInTheDocument();
+  });
+  
+  test('parses the url text correctly', () => {
+    expect(parseUrlFromText(mockTweet.text)).toEqual('https://t.co/ABCdeFG87J');
+  });
+
+  test('displays the avatar', () => {
+    const tweetAvatar = screen.getByAltText(`${mockTweet.user.screen_name}'s Twitter avatar`);
+    expect(tweetAvatar).toBeInstanceOf(HTMLImageElement);
+  });
+
+
+})
